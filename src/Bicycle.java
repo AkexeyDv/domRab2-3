56415a2d5f4222ce415a8547df202c949e343a50
AlexDv <DvoinikovAV@yandex.ru>
2024-03-14 10:23:32 +0300

    Bycycle

--- src/Bicycle.java
+++ src/Bicycle.java
@@ -1,6 +1,18 @@
 public class Bicycle {
+    /*Было
+    public String modelName;
+    public int wheelsCount;
+    Исправляем инкапсуляцию
+    */
     private String modelName;
     private int wheelsCount;
+    //Поля класса теперь приватные и доступа извне к ним нет. Поэтому...
+    // Добавляем конструктор
+
+    public Bicycle(String modelName, int wheelsCount) {
+        this.modelName = modelName;
+        this.wheelsCount = wheelsCount;
+    }
 
     public void updateTyre() {
         System.out.println("Меняем покрышку");
