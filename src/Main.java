03bf1ab62e9240259258abafa07a16bbfad37921
AlexDv <DvoinikovAV@yandex.ru>
2024-03-14 19:05:47 +0300

    Укоротим майн

--- src/Main.java
+++ src/Main.java
@@ -1,24 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        Transport car1=new Car("Lada Kalina", 4);
-        Transport bicycle1=new Bicycle("Горный",2);
-        Transport truck1=new Truck("Газель",4);
-        Transport car2=new Car("Ford", 4);
-        Transport bicycle2=new Bicycle("Stels",2);
-        Transport truck2=new Truck("Man",8);
+        Transport[] transports={new Car("Lada Kalina", 4),
+        new Bicycle("Горный",2),
+        new Truck("Газель",4),
+        new Car("Ford", 4),
+        new Bicycle("Stels",2),
+        new Truck("Man",8)};
         //car.check();
-        car1.check();
-        linePrint();
-        car2.check();
-        linePrint();
-        bicycle1.check();
-        linePrint();
-        bicycle2.check();
-        linePrint();
-        truck1.check();
-        linePrint();
-        truck2.check();
-        System.out.println();
+        for (Transport transport : transports) {
+            transport.check();
+            linePrint();
+
+        }
+
+
+
     }
     public static void linePrint(){
         System.out.println("___________________________________");
